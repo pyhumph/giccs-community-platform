e622a38a69f955e92512549981a158b0a6bedf9d
polyhistor2050 <chrisspaul2050@gmail.com>
2025-04-17 14:21:33 +0300

    Update newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Newsletter = () => {
   return (
-    <div className="bg-gray-500 text-white py-12">
+    <div className="bg-amber-100 text-white py-12">
       <div className="container mx-auto px-4">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-6 md:mb-0">
-            <h2 className="text-2xl font-bold mb-2">
+            <h2 className="text-3xl font-bold mb-2 text-gray-700">
               Subscribe To Our Newsletter
             </h2>
-            <p className="text-gray-400">
+            <p className="text-gray-600">
               Regular Inspections And Feedback Mechanisms
             </p>
-            <div className="text-yellow-500 text-4xl mt-4">❤</div>
+            <div className="text-yellow-500 text-5xl mt-4">❤</div>
           </div>
           <div className="w-full md:w-1/2">
             <div className="flex">
               <input
                 type="email"
-                className="w-full px-4 py-3 rounded-l-lg focus:outline-none"
+                className="w-full m-2 px-4 py-4 bg-white text-gray-600 text-lg border border-gray-400 rounded-full focus:outline-none"
                 placeholder="Enter Email"
               />
-              <button className="bg-yellow-500 text-white px-6 py-3 rounded-r-lg hover:bg-yellow-600 transition-colors">
-                →
+              <button className="bg-yellow-500 m-2 text-white text-lg px-6 py-3 rounded-full hover:bg-yellow-600 transition-colors">
+                Subscribe
               </button>
             </div>
           </div>
